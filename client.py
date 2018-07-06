5287108750d9c0e475e20bb9f2f0f9654a9941b4
stkishore <40885576+stkishore@users.noreply.github.com>
2018-07-06 14:23:35 +0530

    Create client.py

---
+++ client.py
@@ -0,0 +1,7 @@
+import socket
+UDP_IP_ADDRESS="127.0.0.1"
+UDP_PORT_NUMBER=6743
+Message=("server reply received")
+bytesToSend = str.encode(Message)
+clientSock = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
+clientSock.sendto(bytesToSend ,(UDP_IP_ADDRESS,UDP_PORT_NUMBER))
